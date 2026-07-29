32896a997df7d22c4f6e5a8775b58a6c9762ad71
Sienoke <95586739+Sienoke@users.noreply.github.com>
2026-07-30 01:21:12 +0300

    Update __init__.py

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,4 +1,2 @@
 from .parser_txt import parse_bank_statement_txt
-from .parser_docx import parse_bank_statement_docx
-from .parser_xlsx import parse_bank_statement_xlsx
 from .parser_csv import parse_bank_statement_csv
